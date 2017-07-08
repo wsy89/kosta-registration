c994a45daa0fa77a5d6700b6c55381288920d84d
Leanne Won <wonleanne@gmail.com>
2017-07-08 21:13:17 +1200

    initial commit for kosta choose modal, few styling change

--- src/app/modal/choose-kosta-modal.component.ts
+++ src/app/modal/choose-kosta-modal.component.ts
@@ -1,11 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {WindowRefService} from '../service/window-ref.service';
 
 @Component({
   selector: 'choose-kosta',
-  templateUrl: 'choose-kosta-modal.html'
+  templateUrl: 'choose-kosta-modal.html',
 })
-export class ChooseKostaModalComponent implements OnInit {
-  ngOnInit() {
+export class ChooseKostaModalComponent {
+  private _window: Window;
+  private topPosition: number;
 
+  constructor(private winRef : WindowRefService) {
+    this._window = winRef.nativeWindow;
+    this.topPosition = this._window.innerHeight / 2;
   }
 }
