6e561a981a2a886ed3d66db981dafa7ca268a6fc
Leanne Won <wonleanne@gmail.com>
2017-08-29 19:44:27 +1200

    set modal top margin

--- src/app/modal/choose-kosta-modal.component.ts
+++ src/app/modal/choose-kosta-modal.component.ts
@@ -5,19 +5,11 @@ import {WindowRefService} from '../service/window-ref.service';
   selector: 'choose-kosta',
   templateUrl: 'choose-kosta-modal.html',
 })
-export class ChooseKostaModalComponent implements AfterViewInit{
+export class ChooseKostaModalComponent {
 
-  @ViewChild('chooseModal') chooseModal: ElementRef;
   private _window: Window;
-  topPosition: number = 200;
 
   constructor(private winRef : WindowRefService) {
     this._window = winRef.nativeWindow;
-  }
-
-  ngAfterViewInit(){
-    // var modalHeight = this.chooseModal.nativeElement.offsetHeight;
-    // this.topPosition = (this._window.innerHeight - modalHeight) / 2;
-
   }
 }
