4c534423f92f9b546af65e6504abd8406f94351b
Leanne Won <wonleanne@gmail.com>
2017-09-02 17:30:05 +1200

    update header section

--- src/app/common/header.component.ts
+++ src/app/common/header.component.ts
@@ -1,17 +1,24 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {WindowRefService} from '../service/window-ref.service';
 
 @Component({
   selector: 'header',
   templateUrl: 'header.html'
 })
 export class HeaderComponent implements OnInit {
-
   @Input() title;
   @Input() subTitle;
   @Input() date;
   @Input() location;
+  _window: WindowRefService;
+  minHeight: number;
+
+  constructor(private winRef: WindowRefService) {
+    this._window = winRef;
+    this.minHeight = this._window.nativeWindow.innerHeight;
+  }
 
   ngOnInit() {
-
   }
+
 }
