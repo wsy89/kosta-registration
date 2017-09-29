655f933577c529de53f2342a52a7580962cf34a4
nick ko <chpa6425@hotmail.com>
2017-09-29 23:00:06 +1300

    Updated description

--- src/app/kosta-nz.component.ts
+++ src/app/kosta-nz.component.ts
@@ -108,12 +108,12 @@ export class KostaComponent implements OnInit {
   }
 
   private loadSpeakers() {
-    this.speakers.push(new Speaker('assets/img/speakers/speaker1.jpg', '윤은성 강사', '', ''));
-    this.speakers.push(new Speaker('assets/img/speakers/K_kimdohyun.jpg', '김도현 강사', '', 'Colorado Christian University 고등학교 졸업 후 가족과 미국으로 건너가 LA에서 이민생활을 시작했고, 나성영락교회 대학부 시절 하나님의 부르심을 받았다. UCLA에서 역사를 공부하고 Fuller (MDiv), Yale (STM)에서 공부한 후 Union 신학교에서 신약학으로 Ph.D.를 받았다. Sterling College에서 종교학 과장을 역임했으며 현재 Denver에 있는 Colorado Christian University에서 신약학과 헬라어를 가르치고 있다. 또한 장로교 목사로 볼더한인교회를 담임하고 있다. 저서로는 <나의 사랑하는 책 로마서>, <빌립보서 어떻게 읽을 것인가>가 있다.'));
-    this.speakers.push(new Speaker('assets/img/speakers/K_kimchanggeun.jpg', '김창근 강사', '', ''));
-    this.speakers.push(new Speaker('assets/img/speakers/K_chaedonguk.jpg', '채종욱 강사', '', 'GBM Korea / GBM Asia (인도) 그룹회장이며, 미주 GMP 선교회 이사장과 사업체와 비영리구호단체인 빈손채움재단의 이사장이기도 하다. 비즈니스를 통해 선교적 삶을 살아가는 대안적 선교방식을 알리고 있다. 비즈니스선교모델을 개발하고 선교지에 적용하는 노력을 기울이며, 관심자들을 적극적으로 돕고 있다.'));
-    this.speakers.push(new Speaker('assets/img/speakers/speaker1.jpg', '박동희 강사', '', ''));
-    this.speakers.push(new Speaker('assets/img/speakers/speaker1.jpg', '서재석 강사', '', ''));
+    this.speakers.push(new Speaker('assets/img/speakers/speaker1.jpg', '윤은성 강사', '', '이름없는 교회 담임. 한국 어깨동무사역원 대표'));
+    this.speakers.push(new Speaker('assets/img/speakers/K_kimdohyun.jpg', '김도현 강사', '', 'Colorado Christian College 교수'));
+    this.speakers.push(new Speaker('assets/img/speakers/K_kimchanggeun.jpg', '김창근 강사', '', '무학교회 담임'));
+    this.speakers.push(new Speaker('assets/img/speakers/K_chaedonguk.jpg', '채종욱 강사', '', 'GMP 선교사'));
+    this.speakers.push(new Speaker('assets/img/speakers/speaker1.jpg', '박동희 강사', '', 'GMP 선교사'));
+    this.speakers.push(new Speaker('assets/img/speakers/speaker1.jpg', '서재석 강사', '', 'Young2080 대표'));
   }
 
   private loadSchedules() {
